26690ef072f8928a9d77115a2d4cb21ded74be29
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-03-12 21:56:38 +0530

    added oras
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- oci.go
+++ oci.go
@@ -1,13 +1,58 @@
 package oci
 
-func ORAS_demo(reg string) error {
-	return nil
+import (
+	"context"
+
+	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/registry/remote"
+	"oras.land/oras-go/v2/registry/remote/auth"
+	"oras.land/oras-go/v2/registry/remote/retry"
+)
+
+func ORAS_demo() error {
+	ctx := context.Background()
+	username := "username"
+	password := "password"
+
+	// Setting up local repository
+	reg := "example.registy.io"
+	repo, err := remote.NewRepository(reg + "/myrepo")
+	if err != nil {
+		panic(err)
+	}
+	repo.Client = &auth.Client{
+		Client: retry.DefaultClient,
+		Cache:  auth.DefaultCache,
+		Credential: auth.StaticCredential(reg, auth.Credential{
+			Username: username,
+			Password: password,
+		}),
+	}
+
+	// Seting up source repository to pull from
+	src, err := remote.NewRepository(reg + "/source")
+	if err != nil {
+		panic(err)
+	}
+
+	// Pulling from source to local repository
+	repoDescriptor, err := oras.Copy(ctx, src, "latest", repo, "latest", oras.DefaultCopyOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	// Fetch using the digest
+	repoDescriptor, readCloser, err := oras.Fetch(ctx, repo, repoDescriptor.Digest.String(), oras.DefaultFetchOptions)
+	readCloser.Close()
+
+	// Sign the image using notation
+
+	// Fetch the signed image using ORAS
+
+	// Verify that the image is signed properly using Local Verification
+	return err
 }
 
-func Fluxcd_demo(reg string) error {
-	return nil
-}
-
-func Regctl_demo(reg string) error {
+func Fluxcd_demo() error {
 	return nil
 }
--- oci_test.go
+++ oci_test.go
@@ -9,7 +9,3 @@ func Test_ORAS_demo(t *testing.T) {
 func Test_flux_demo(t *testing.T) {
 
 }
-
-func Test_Regctl_demo(t *testing.T) {
-
-}
